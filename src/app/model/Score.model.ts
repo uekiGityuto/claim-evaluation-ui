2e70d95223f3c0028f674ccad4fd171c4c3b9e9e
Jommyon.Lee@ibm.com <Jommyon.Lee@ibm.com>
2020-05-27 09:29:23 +0900

    footer

--- src/app/model/Score.model.ts
+++ src/app/model/Score.model.ts
@@ -7,33 +7,39 @@ import { logging } from 'protractor';
 export class Score {
     fraudScoreId: string;
     score: number;
-    createDate: Date;
     claimId: string;
     claim: Claim;
     reasons: Reason[];
     feedback: Feedback;
+    history: Score[];
+    isFraud: boolean;
+    createDate: Date;
+    updateDate: Date;
 
     constructor(fraudScoreId: string = '',
                 score: number = null,
-                createDate: string = '',
                 claimId: string = '',
                 claim: Claim = new Claim(),
                 reasons: Reason[] = [],
-                feedback: Feedback = new Feedback()) {
+                feedback: Feedback = new Feedback(),
+                history: Score[] = [],
+                createDate: Date = null,
+                updateDate: Date = null) {
         this.fraudScoreId = fraudScoreId;
         this.score = score;
         this.claimId = claimId;
-        this.createDate = new Date(createDate);
         this.claim = claim;
         this.reasons = reasons;
         this.feedback = feedback;
+        this.history = history;
+        this.createDate = new Date(createDate);
+        this.updateDate = new Date(updateDate);
     }
 
     setRequestsData(data: object) {
         this.fraudScoreId = data['fraudScoreId'.toString()];
         this.score = data['score'.toString()];
         this.claimId = data['claimId'.toString()];
-        this.createDate = data['createDate'.toString()];
         this.claim = data['claim'.toString()];
         this.reasons = data['reasons'.toString()];
         this.feedback = data['feedback'.toString()];
@@ -44,5 +50,15 @@ export class Score {
         if (this.feedback.fraudScoreId === null || this.feedback.fraudScoreId === undefined || this.feedback.fraudScoreId === '') {
           this.feedback.fraudScoreId = this.fraudScoreId;
         }
+        this.history = data['history'.toString()];
+        this.history.forEach(s => {
+          if (s.score >= 700) {
+            s.isFraud = true;
+          } else {
+            s.isFraud = false;
+          }
+        });
+        this.createDate = data['createDate'.toString()];
+        this.updateDate = data['updateDate'.toString()];
     }
 }
