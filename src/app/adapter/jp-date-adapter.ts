5324380026fabef65623f4d37af521219030539f
Yuto Ueki <Yuto.Ueki@ibm.com>
2020-09-12 09:24:32 +0900

    waring解消

--- src/app/adapter/jp-date-adapter.ts
+++ src/app/adapter/jp-date-adapter.ts
@@ -1,7 +1,16 @@
+import { Injectable } from '@angular/core';
 import { NativeDateAdapter } from '@angular/material/core';
 
+/**
+ * カレンダに表示する日付表記を(d日ではなく)dにする
+ * @author SKK231527 植木
+ */
+@Injectable({
+  providedIn: 'root',
+})
 export class JPDateAdapter extends NativeDateAdapter {
   getDateNames(): string[] {
-    return Array.from(Array(31), (v, k) => `${k + 1}`);
+    // * '1'から'31'までの連番を配列で返す
+    return Array.from(Array(31), (value, key) => `${key + 1}`);
   }
 }
