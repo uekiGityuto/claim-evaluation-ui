a3e1270430de5e06f4ccfd21edece4539c2fa0fb
Yuto Ueki <Yuto.Ueki@ibm.com>
2020-10-02 11:28:52 +0900

    レイアウト修正

--- src/app/model/scores/fraud-score.ts
+++ src/app/model/scores/fraud-score.ts
@@ -2,7 +2,6 @@ import { ScoreDetail } from './score-detail';
 import { ScoreCategory } from './score-category';
 
 export interface FraudScore {
-  // TODO: serverからはstringで受け取る。serverのgsonで型変換するかui側で型変換するかは要検討
   scoringDate: Date;
   claimCategory: string;
   scoreDetail: ScoreDetail[];
