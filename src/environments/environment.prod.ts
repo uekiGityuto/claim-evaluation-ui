211b2913086d11b22d2e9ade16b18645d22fdc48
Yuto Ueki <Yuto.Ueki@ibm.com>
2020-10-15 12:35:22 +0900

    環境変数ファイルを元に戻す

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
-// デフォルト設定（environment.ts）を本番用にオーバライドする設定
-
 export const environment = {
   production: true,
-  authorize_url: '../api/authorize/authorize',
-  scores_url: '../api/scores',
-  transition_url: '../api/claims/transition',
-  claims_url: '../api/claims/get',
-  // TODO: ヘルプページのリンクは修正する
-  help_url: 'https://angular.jp/docs'
 };
