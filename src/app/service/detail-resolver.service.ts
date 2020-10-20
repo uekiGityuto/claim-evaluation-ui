8aea01704c5e837106a24cd5069fd3124f5b59f7
Yuto Ueki <Yuto.Ueki@ibm.com>
2020-10-20 11:42:31 +0900

    エラーのままだが確認したことを残す意味でコミット

--- src/app/service/detail-resolver.service.ts
+++ src/app/service/detail-resolver.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of, EMPTY } from 'rxjs';
+import { mergeMap, take } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 import { Scores } from '../model/scores/scores';
 
@@ -15,10 +17,21 @@ import { ScoresClientService } from './scores-client.service';
 })
 export class DetailResolverService implements Resolve<Scores> {
 
-  constructor(private client: ScoresClientService) { }
+  constructor(private client: ScoresClientService, private router: Router) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Scores> {
     const claimNumber = route.paramMap.get('claimNumber');
-    return this.client.post(claimNumber);
+    return this.client.post(claimNumber).pipe(
+      take(1),
+      mergeMap(response => {
+        console.log('受信確認');
+        if (response) {
+          return of(response);
+        } else {
+          this.router.navigate(['/list/error']);
+          return EMPTY;
+        }
+      })
+    );
   }
 }
